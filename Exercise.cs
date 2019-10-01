b3c002671f1ce64073f434a849e97d5201a7da6c
RJHGO <leftantenna@msn.com>
2019-09-30 19:50:04 -0700

    C# Exercise

---
+++ Exercise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember," + " We're " + "accounting" + " on you!");
+            Console.ReadLine();
+        }
+    }
+}
