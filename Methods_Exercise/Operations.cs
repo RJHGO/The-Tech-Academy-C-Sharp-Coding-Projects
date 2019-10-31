7199f0509cf3d4ce25757686b8f4b4d5aac4f82b
RJHGO <53460219+RJHGO@users.noreply.github.com>
2019-10-30 20:00:20 -0700

    Add files via upload
    Methods_Exercise_for_the_Tech_Academy_boot_camp.

---
+++ Methods_Exercise/Operations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Exercise
+{
+    class Operations
+    {
+        static public int AddIt(int UserNum)
+        {
+            int answer1;
+            answer1 = UserNum + 5;
+            return answer1;
+        }
+        static public int TimesIt(int UserNum)
+        {
+            int answer2;
+            answer2 = UserNum * 5;
+            return answer2;
+        }
+        static public int DivideIt(int UserNum)
+        {
+            int answer3;
+            answer3 = UserNum / 5;
+            return answer3;
+        }
+    }
+}
---
+++ Methods_Exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int UserNum, final1, final2, final3;
+            Console.WriteLine("What Number would you like to do math operations on?");
+            UserNum = int.Parse(Console.ReadLine());
+            final1 = Operations.AddIt(UserNum);
+            Console.WriteLine("Your number + 5: " + final1);
+            final2 = Operations.TimesIt(UserNum);
+            Console.WriteLine("Your number x 5: " + final2);
+            final3 = Operations.DivideIt(UserNum);
+            Console.WriteLine("Your number divided by 5: " + final3);
+            Console.ReadLine();
+       
+        }
+    }
+}
