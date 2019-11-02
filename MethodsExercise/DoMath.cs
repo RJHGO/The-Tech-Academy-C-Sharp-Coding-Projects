8ede4ba566b33c54cca1a3b9c7d4f7d61a578a94
RJHGO <53460219+RJHGO@users.noreply.github.com>
2019-11-02 11:58:06 -0700

    Add files via upload
    MethodsExercise2

---
+++ MethodsExercise/DoMath.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    class DoMath
+    {
+        public static int MathOp1(int UserNum)
+        {
+            int Result1;
+            Result1 = UserNum + 10;
+            return Result1;
+        }
+        public static double MathOp1(double UserNum1)
+        {
+            double Result2;
+            Result2 = UserNum1 / 2;
+            return Result2;
+        }
+        public static uint MathOp1( uint UserNum2)
+        {
+            string Result3 = "5";
+            uint parsedResult3;
+            uint.TryParse(Result3, out parsedResult3);
+            uint Display = parsedResult3 * UserNum2;
+            return Display;
+        }
+
+    }
+}
---
+++ MethodsExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Your chosen number + 10");
+            int UserNum = int.Parse(Console.ReadLine());
+            int Solve1 = DoMath.MathOp1(UserNum);
+            Console.WriteLine(Solve1);
+           
+            Console.WriteLine("Your chosen decimal number divided by 2");
+            double UserNum1 = double.Parse(Console.ReadLine());
+            double Solve2 = DoMath.MathOp1(UserNum1);
+            Console.WriteLine(Solve2);
+            
+
+            Console.WriteLine("Your chosen number * 5");
+            uint UserNum2 = uint.Parse(Console.ReadLine());
+            uint Solve3 = DoMath.MathOp1(UserNum2);
+            Console.WriteLine(Solve3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
