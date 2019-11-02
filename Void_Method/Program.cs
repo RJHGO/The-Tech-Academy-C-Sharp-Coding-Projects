461b216059bf2a4cd3834634ca4f579e53e8c8e5
RJHGO <53460219+RJHGO@users.noreply.github.com>
2019-11-01 19:58:17 -0700

    Add files via upload
    Void_Method_Exercise

---
+++ Void_Method/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstNum = 0, SecNum = 0;
+            Console.WriteLine(" Enter your first number to multiply");
+            FirstNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter you second number to multiply");
+            SecNum = int.Parse(Console.ReadLine());
+
+            VoidMethodInts voidmethodsint = new VoidMethodInts();
+            VoidMethodInts.Nums(FirstNum, SecNum);
+ 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Void_Method/VoidMethodInts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    class VoidMethodInts
+    {
+        public static void Nums(int NumOne, int NumTwo)
+        {
+            int Operate;
+            Operate = NumOne * NumTwo;
+
+            
+        }
+         
+    }
+}
