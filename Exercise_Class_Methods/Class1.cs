c9e7b2fe5872df34e76b15682227f48da9ea77dc
RJHGO <53460219+RJHGO@users.noreply.github.com>
2019-11-04 16:33:23 -0800

    Add files via upload
    Classes_&_Methods

---
+++ Exercise_Class_Methods/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Class_Methods
+{
+    class Class1
+    {
+        public int Optional(int uNum1, int uNum2 = 0)
+        {
+            int x = 10;
+            if(uNum1 > 0 )
+            {
+                return uNum1 * x;
+            }
+            else
+            {
+                return uNum2 * x;
+            }
+            
+        }     
+    }
+}
---
+++ Exercise_Class_Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Class_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            int uNum1, uNum2, results, results1;
+
+            Console.WriteLine("Please enter two numbers one at a time, at least one number is required.");
+            Console.WriteLine("Enter first number: ");
+            string Entered1 = Console.ReadLine();
+            uNum1 = int.Parse(Entered1);
+            results = class1.Optional(uNum1);
+            Console.WriteLine(results);
+            
+            Console.WriteLine("Enter second number_optional...: ");
+
+            if(int.TryParse(Console.ReadLine(), out uNum2))
+            {
+                results1 = class1.Optional(uNum2);
+                Console.WriteLine(results1);
+                Console.ReadLine();
+            }
+           
+        }
+    }
+}
