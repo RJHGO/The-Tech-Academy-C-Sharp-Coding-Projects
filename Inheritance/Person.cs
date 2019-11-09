070b8908135b443f77d899c70e19ee3a67157b01
RJHGO <53460219+RJHGO@users.noreply.github.com>
2019-11-09 11:20:51 -0800

    Add files via upload
    Inheritance_Exercise

---
+++ Inheritance/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Person
+    {
+        public List<string> Names { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public void SayName()
+        {
+            foreach (string name in Names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
