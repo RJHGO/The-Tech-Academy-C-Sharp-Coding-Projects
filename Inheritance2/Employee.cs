bee2e6df1af52185f1a1350d1713cf6a3e2510f3
RJHGO <53460219+RJHGO@users.noreply.github.com>
2019-11-09 18:06:46 -0800

    Add files via upload
    Inheritance_2_Abstract_Methods

---
+++ Inheritance2/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance2
+{
+    class Employee : Person
+    {
+        public override void ListNames()
+        {
+            int ID;
+        }
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+    }
+}
+  
+
---
+++ Inheritance2/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance2
+{
+    public abstract class Person
+    {
+        public List<string> Names { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract void ListNames();
+        public virtual void SayName()
+        {
+            foreach (string name in Names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
