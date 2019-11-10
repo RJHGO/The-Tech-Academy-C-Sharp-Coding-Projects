078ac8cd6bce3245f88c4783371121e0b399bb6d
RJHGO <53460219+RJHGO@users.noreply.github.com>
2019-11-10 12:42:16 -0800

    Add files via upload
    Interface_Exercise

---
+++ Interface_eX/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_eX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Employee();
+            person.FirstName = "Sample";
+            person.LastName = "Student";
+            person.Quit();
+            //Person.SayName();
+            Console.ReadLine();
+
+            
+            
+
+
+           
+        }
+    }
+}
