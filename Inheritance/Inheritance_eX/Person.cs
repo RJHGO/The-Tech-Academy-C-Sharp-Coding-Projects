a0fcb2e012806e5a08b82827db20b245938c530b
RJHGO <53460219+RJHGO@users.noreply.github.com>
2019-11-09 22:56:13 -0800

    Add files via upload
    Inheritance_Exercise

---
+++ Inheritance/Inheritance_eX/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_eX
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public void SayName()
+        {
+           
+            Console.WriteLine("Name: " + FirstName  + " " +  LastName);
+        }
+        
+        //This is close, but you aren't setting the first and last name values to the properties of the object, 
+        //you are just creating a list and printing off all of the elements of that.  
+        //Remove the List property and just use the .Firstname and .Lastname.
+        //Then resubmit.
+        //-Jordan
+    }
+}
---
+++ Inheritance/Inheritance_eX/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_eX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee Person = new Employee();
+            Person.FirstName = "Sample";
+            Person.LastName = "Student";
+            Person.SayName();
+            Console.ReadLine();
+
+           
+        }
+    }
+}
