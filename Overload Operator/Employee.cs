cf166b7b604686d0ea41bed298490b78fd090651
RJHGO <leftantenna@msn.com>
2019-11-16 11:54:30 -0800

    Overloading operators

---
+++ Overload Operator/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload_Operator
+{
+   class Employee
+    {
+
+        int z;
+        
+        public Employee(int EmployeeId)
+        {
+            z = EmployeeId;
+        }  
+        public static bool operator == (Employee x, Employee y)
+        {
+            return x.z == y.z;
+        }
+        public static bool operator != (Employee x, Employee y)
+        {
+            return x.z != y.z;
+        }
+    }
+}
---
+++ Overload Operator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload_Operator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Employee a  = new Employee(2015);
+            Employee b = new Employee(2019);
+            
+
+            Console.WriteLine(a == b);
+            Console.ReadLine();
+
+
+
+           
+        }
+      }
+    }
+
