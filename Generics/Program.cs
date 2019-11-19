1e9be6587d84d69549b8e203f0a83896220e2299
RJHGO <leftantenna@msn.com>
2019-11-18 18:45:15 -0800

    Generics Exercise

---
+++ Generics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+            
+        static void Main(string[] args)
+        {
+            Employee<int> employee = new Employee<int>();
+            employee.Things = new List<int>(){2, 4, 6, 8, 10};
+
+            foreach (int Thing in employee.Things)
+            {                
+                    Console.WriteLine(Thing);
+                                   
+            }
+
+            Employee<string> employee1 = new Employee<string>();
+            employee1.Things = new List<string>() { "Henry", "Linda", "Kelly", "Mark", "Tom" };
+
+            foreach (string Thing in employee1.Things)
+            {
+                Console.WriteLine(Thing);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
