e356798e658130a40e8cc27218889fc20646b6bc
RJHGO <leftantenna@msn.com>
2019-11-19 19:37:45 -0800

    Struct_Exercise

---
+++ Struct_Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Exercise
+{
+    class Program
+    {
+        public struct Number
+        {
+            public decimal Amount;   
+        }
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 12.6M;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
