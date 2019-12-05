0c0231ae5293ccef0d8b950bfb9eea6d4bf6acad
RJHGO <leftantenna@msn.com>
2019-12-04 20:49:57 -0800

    logging text from user

---
+++ Log/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give a number");
+            int UserNum = Convert.ToInt32(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\lefta\Desktop\Rons\log.txt", Convert.ToString(UserNum));
+            string UserNum1 = File.ReadAllText(@"C:\Users\lefta\Desktop\Rons\log.txt");
+            Console.WriteLine(UserNum1);
+            Console.ReadLine();
+            
+        }
+    }
+}
