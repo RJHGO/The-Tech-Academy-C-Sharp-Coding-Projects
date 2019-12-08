067b4f6b060a558e42b996f79218b3b82357b319
RJHGO <leftantenna@msn.com>
2019-12-08 12:17:10 -0800

    date and time exercise.

---
+++ dates_n_Time/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace dates_n_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Give a Number.");
+            int UserNum = Convert.ToInt32(Console.ReadLine());
+            DateTime thisTime = DateTime.Now;
+            DateTime plusHrs = thisTime.AddHours(UserNum);
+            Console.WriteLine(plusHrs);
+            Console.ReadLine();
+        }
+    }
+}
