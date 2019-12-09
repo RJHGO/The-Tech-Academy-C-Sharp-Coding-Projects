acf93990f9e01eaf13b317b2603d8052ea8f240a
RJHGO <leftantenna@msn.com>
2019-12-09 14:37:36 -0800

    ChainedConstructor

---
+++ Chained/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chained
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newCARS = new TheCars();
+            int numOfCARS = (newCARS.NumOfCars);
+            string bigName = newCARS.Brand;
+
+            
+            Console.WriteLine(Convert.ToString(numOfCARS) +" "+ bigName);
+            Console.ReadLine();
+            
+
+            
+            
+            
+           
+        }
+    }
+}
---
+++ Chained/TheCars.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chained
+{
+    class TheCars
+    {
+        public int NumOfCars;
+        public string Brand;
+
+        public TheCars() : this(10, "Ford")
+        {
+        }
+
+        public TheCars(int NumOfCars, string Brand)
+        {
+            this.NumOfCars = NumOfCars;
+            this.Brand = Brand;
+        }
+
+    }
+}
