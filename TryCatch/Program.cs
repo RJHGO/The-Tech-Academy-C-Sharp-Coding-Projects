9f1b3ef2671c7965bf1ddfa1677ff1b526fdb00f
RJHGO <leftantenna@msn.com>
2019-12-09 21:55:28 -0800

    try&catch

---
+++ TryCatch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter your age number, no decimals.");
+                int UserAge = Convert.ToInt32(Console.ReadLine());
+                DateTime date = DateTime.Now;
+                var date1 = date.AddYears(-UserAge);
+                var born = date1.Date.Year;
+                Console.WriteLine(born);
+                Console.ReadLine();
+                
+            }
+            catch(NotFiniteNumberException)
+            {
+                Console.WriteLine("Enter a number greater than 0 for your age.");
+                Console.ReadLine();
+                return;
+            }
+
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("You did not give a valid number for age.");
+                Console.ReadLine();
+                return;
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("You did not give a valid number for age.");
+                Console.ReadLine();
+                return;
+            }
+           
+                     
+                
+            
+
+        }
+    }
+}
